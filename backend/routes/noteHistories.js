a30f5c759a9eac8c73a152aef59ca4ff8187b198
s18899pjatk <s18899@pjwstk.edu.pl>
2021-02-11 16:48:15 +0100

    added note version logic to web-service

--- backend/routes/noteHistories.js
+++ backend/routes/noteHistories.js
@@ -1,5 +1,4 @@
 const { NoteHistory } = require("../models/noteHistory");
-const Joi = require("joi");
 const express = require("express");
 const router = express.Router();
 const validateId = require("../middlewares/validateId");
@@ -19,57 +18,5 @@ router.get("/:id", validateId, async (req, res) => {
 
   res.send(notehist);
 });
-
-router.post("/", async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const noteHistory = new NoteHistory({
-    noteId: req.body.noteId,
-    notes: req.body.notes,
-  });
-
-  await noteHistory.save();
-  res.send(noteHistory);
-});
-
-router.put("/:id", validateId, async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const noteHistory = await NoteHistory.findByIdAndUpdate(
-    req.params.id,
-    {
-      noteId: req.body.noteId,
-      notes: req.body.notes,
-    },
-    { new: true }
-  );
-
-  if (!noteHistory)
-    return res.status(404).send("The note with the given ID was not found.");
-
-  res.send(noteHistory);
-});
-
-router.delete("/:id", validateId, async (req, res) => {
-  const noteHistory = await NoteHistory.findByIdAndRemove(req.params.id);
-
-  if (!noteHistory)
-    return res
-      .status(404)
-      .send("The note history with the given ID was not found.");
-
-  res.send(noteHistory);
-});
-
-function validate(noteHistory) {
-  const schema = {
-    noteId: Joi.string().required(),
-    notes: Joi.array().required(),
-  };
-
-  return Joi.validate(noteHistory, schema);
-}
 
 module.exports = router;
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,11 @@
 const { Note } = require("../models/note");
+const { NoteHistory } = require("../models/noteHistory");
+
 const Joi = require("joi");
 const express = require("express");
 const router = express.Router();
 const validateId = require("../middlewares/validateId");
+const _ = require("lodash");
 
 router.get("/", async (req, res) => {
   const notes = await Note.find().select("-__v").sort("title");
@@ -22,12 +25,22 @@ router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  // creating note
   const note = new Note({
     title: req.body.title,
     content: req.body.content,
   });
 
   await note.save();
+
+  // creating the history tracking for the note
+  const noteHistory = new NoteHistory({
+    noteId: note._id,
+    notes: [],
+  });
+
+  await noteHistory.save();
+
   res.send(note);
 });
 
@@ -35,18 +48,26 @@ router.put("/:id", validateId, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const oldNote = await Note.findById(req.params.id).select("-__v");
+  const oldNote = await Note.findById(req.params.id);
 
+  // updating the note
   const note = await Note.findByIdAndUpdate(
     req.params.id,
     {
       title: req.body.title,
       content: req.body.content,
-      version: ++oldNote.version,
+      version: oldNote.version + 1,
       modified: Date.now(),
     },
     { new: true }
   );
+
+  // adding previos version of note to it's history
+  NoteHistory.findOne({ noteId: oldNote._id }).then((h) => {
+    h.noteId = h.noteId;
+    h.notes = _.concat(h.notes, oldNote);
+    h.save();
+  });
 
   if (!note)
     return res.status(404).send("The note with the given ID was not found.");
@@ -55,7 +76,15 @@ router.put("/:id", validateId, async (req, res) => {
 });
 
 router.delete("/:id", validateId, async (req, res) => {
+  // deleting note
   const note = await Note.findByIdAndRemove(req.params.id);
+
+  // deleting it's history
+  NoteHistory.findOneAndRemove({ noteId: note._id }, (err, docs) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 
   if (!note)
     return res.status(404).send("The note with the given ID was not found.");
